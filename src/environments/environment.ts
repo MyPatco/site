d28452fdfaa69ca9f6e615f89c184837ae7f664c
Anthony <fassetar@gmail.com>
2019-10-14 11:37:27 -0400

    updated new assets and fixes for home page.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB1wfc7SScusyylypnMZOA2U0kmmDgZp7w",
-    authDomain: "mypatco-beta.firebaseapp.com",
-    databaseURL: "https://mypatco-beta.firebaseio.com",
-    projectId: "mypatco-beta",
-    storageBucket: "",
-    messagingSenderId: "478306198823",
-    appId: "1:478306198823:web:b0a7fbf1001c39fff57502"
+    //YOUR CODE
   }
-};
+}; 
